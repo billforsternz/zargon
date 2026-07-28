a2b57b694d67158f1cc2c04e89a9841ded9c9dbe
Bill Forster <billforsternz@gmail.com>
2026-07-28 12:24:50 +1200

    Install and test z80 fix for the ATKSAV_BUG in Zargon's sargon-86 directory (neglected and broken in Zargon, so we had to make a 'sargon-86-still-works-okay-note-that-newer-zargon-breaks-it branch from commit 4586a87e2). First version of this, this works but we have a much neater and better version in mind, that one comes next

---
+++ sargon-x86/build-sargon-x86.bat
@@ -0,0 +1,14 @@
+REM
+REM Inputs sargon-8080-and-x86.asm (prepared manually from sargon4.asm)
+REM Outputs sargon-z80.asm and sargon-x86.asm
+REM
+convert-8080-to-z80-or-x86.exe -generate_z80_only sargon-8080-and-x86.asm sargon-z80.asm temp.h temp.txt
+convert-8080-to-z80-or-x86.exe -generate_x86 -relax sargon-8080-and-x86.asm sargon-x86.asm temp.h temp.txt
+del temp.h
+del temp.txt
+REM
+REM Next manually merge the significant difference in sargon-x86.asm
+REM into src/sargon-x86.asm which has REM had many manual changes (eg
+REM CALLBACK statements) since it was automatically generated.
+REM
+
